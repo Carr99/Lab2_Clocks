d2258ee67765896a30f054360911db703f3453f4
Carr99 <andre@carrblad.se>
2022-02-21 15:49:24 +0100

    Load all cities to start page
    * Added main.js file to handle button clicks and to change between partials
    * start.html, printAllCities.js added to show all cities from the json file
    * style.css added with styling for the start page, fonts is also imported from google api

---
+++ js/main.js
@@ -0,0 +1,28 @@
+document.querySelector('body').addEventListener('click', function (event) {
+
+  let aButton = event.target.closest('button');
+
+  if (!aButton) { return; }
+
+  event.preventDefault();
+  rotuer(newPage);
+});
+
+async function rotuer(aV) {
+  let route = location.pathname;
+  if (aV != null) {
+    route = aV;
+  }
+  route = route === '/' ? '/start' : route;
+  route = '/partials' + route + '.html';
+  // load partial
+  let content = await (await fetch(route)).text();
+  content.includes('<title>Error</title > ') && location.replace(' / ');
+  document.querySelector('main').innerHTML = content;
+
+  route === '/partials/start.html' && loadJsonAndDisplay();
+}
+
+window.addEventListener('popstate', rotuer);
+
+rotuer();
---
+++ js/printAllCities.js
@@ -0,0 +1,15 @@
+async function loadJsonAndDisplay() {
+  let rawData = await fetch('/cities.json')
+  let citys = await rawData.json()
+  let html = ''
+
+  for (let city of citys) {
+    html += `
+    <Button class="citySquare" id="${city.id}@${city.city}@${city.timezone}" >
+      <p class="cityTitle">${city.city}</p>
+    </Button>
+    `
+  }
+  document.querySelector('.citys').innerHTML = html
+}
+loadJsonAndDisplay();
