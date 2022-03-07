38557e71d489835a87a7442cdc9a0dee9a291ed8
Carr99 <andre@carrblad.se>
2022-03-07 10:09:52 +0100

    Shows cities from localstorage
    * printAllCitites.js now prints all the stored cities in the local storage

--- js/printAllCities.js
+++ js/printAllCities.js
@@ -3,6 +3,17 @@ async function loadJsonAndDisplay() {
   let citys = await rawData.json()
   let html = ''
 
+  let existingCity = JSON.parse(localStorage.getItem("AllCity"));
+  if (existingCity != null) {
+    for (let city of existingCity) {
+      html += `
+    <Button class="citySquare" id="own@${city.city}@${city.timezone}" >
+      <p class="cityTitle">${city.city}</p>
+    </Button>
+    `
+    }
+  }
+
   for (let city of citys) {
     html += `
     <Button class="citySquare" id="${city.id}@${city.city}@${city.timezone}" >
